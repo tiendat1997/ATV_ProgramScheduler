3a291a8bb8b1e8ae7601c4a06e080c11d0723e2a
DATTTSE62330 <tiendatdeveloper1997@gmail.com>
2019-01-30 17:18:30 +0700

    Manage email for admin

--- ATV.ProgramDept.DesktopApp/ManageEmailForm.cs
+++ ATV.ProgramDept.DesktopApp/ManageEmailForm.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using ATV.ProgramDept.Entity;
+using ATV.ProgramDept.Service.Implement;
+using ATV.ProgramDept.Service.Interface;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -12,9 +15,80 @@ namespace ATV.ProgramDept.DesktopApp
 {
     public partial class ManageEmailForm : Form
     {
+        private readonly IDepartmentRepository _deptRepository;
         public ManageEmailForm()
         {
+            _deptRepository = new DepartmentRepository(); 
             InitializeComponent();
         }
+
+        private void ManageEmailForm_Load(object sender, EventArgs e)
+        {
+            // Load departments from database 
+            var depts = _deptRepository.GetAll().ToList();
+            var bindingSource = new BindingSource();
+            bindingSource.DataSource = depts;
+            dgvDepartment.DataSource = bindingSource; 
+        }
+
+        private void btnNewEmail_Click(object sender, EventArgs e)
+        {
+            NewEmailForm newForm = new NewEmailForm(this);
+            newForm.Show(); 
+        }
+
+        private void ManageEmailForm_FormClosed(object sender, FormClosedEventArgs e)
+        {
+            _deptRepository.Dispose(); 
+        }
+        private void dgvDepartment_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+            try
+            {
+                var row = dgvDepartment.Rows[e.RowIndex];
+                var editedDept = (Department)row.DataBoundItem;
+                var dept = _deptRepository.FindById(editedDept.ID);
+                if (dgvDepartment.Columns[e.ColumnIndex].Name.Equals("Edit"))
+                {
+                    // Save processing                                                           
+                    dept.Name = editedDept.Name;
+                    dept.Email = editedDept.Email;
+                    _deptRepository.Save();
+                    MessageBox.Show("Lưu thành công", "Message", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                } 
+                else if (dgvDepartment.Columns[e.ColumnIndex].Name.Equals("Remove"))
+                {
+                    // Remove processing
+                    if (MessageBox.Show("Bạn có chắc muốn xóa email này không ?", "Message", MessageBoxButtons.YesNoCancel, MessageBoxIcon.Question) == DialogResult.Yes)
+                    {
+                        // Processing result;
+                        dept.IsActive = false;
+                        _deptRepository.Save();
+                        MessageBox.Show("Xóa thành công", "Message", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                        dgvDepartment.Rows.RemoveAt(row.Index);
+                        dgvDepartment.Refresh();
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("MANAGE_EMAIL_FORM " + ex.Message);              
+            }
+            finally
+            {
+                dgvDepartment.CurrentCell.Selected = false; 
+            }
+        }
+
+        #region services
+        public void ReloadDepartments()
+        {
+            var depts = _deptRepository.GetAll().ToList();
+            var bindingSource = new BindingSource();
+            bindingSource.DataSource = depts;
+            dgvDepartment.DataSource = bindingSource;
+            dgvDepartment.Refresh(); 
+        }
+        #endregion      
     }
 }
---
+++ ATV.ProgramDept.DesktopApp/NewEmailForm.cs
@@ -0,0 +1,101 @@
+﻿using ATV.ProgramDept.Entity;
+using ATV.ProgramDept.Service.Implement;
+using ATV.ProgramDept.Service.Interface;
+using ATV.ProgramDept.Service.Utilities;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ATV.ProgramDept.DesktopApp
+{
+    public partial class NewEmailForm : Form
+    {
+        private readonly ManageEmailForm manageEmailForm;
+        private readonly IDepartmentRepository _deptRepository;
+        public NewEmailForm(ManageEmailForm parentForm)
+        {
+            manageEmailForm = parentForm;
+            _deptRepository = new DepartmentRepository();
+            InitializeComponent();
+        }
+
+        private void btnCreate_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                string name = txtName.Text.Trim();
+                string email = txtEmail.Text.Trim();
+                bool isValidate = true;
+
+                // validate name 
+                if (ValidationProvider.RequiredStringIsValid(name) == false)
+                {
+                    isValidate = false;
+                    errName.SetError(txtName, "Tên không được rỗng");
+                }
+                else
+                {
+                    this.errName.SetError(txtName, "");
+                }
+
+                // validate email 
+                if (ValidationProvider.RequiredStringIsValid(email) == false)
+                {
+                    isValidate = false;
+                    errEmail.SetError(txtEmail, "Email không được rỗng");
+                }
+                else if (ValidationProvider.RegExStringIsValid(email, RegexPattern.MATCH_EMAIL_PATTERN) == false)
+                {
+                    isValidate = false;
+                    errEmail.SetError(txtEmail, "Email phải theo định dạng [abc@abc.abc]");
+                }
+                else
+                {
+                    this.errEmail.SetError(txtEmail, "");
+                }
+
+                if (isValidate)
+                {
+                    var newDept = new Department
+                    {
+                        Name = name,
+                        Email = email,
+                        IsActive = true
+                    };
+                    _deptRepository.Create(newDept);
+                    _deptRepository.Save();
+                    if (MessageBox.Show("Tạo email thành công!", "Message", MessageBoxButtons.OK, MessageBoxIcon.Information) == DialogResult.OK)
+                    {
+                        manageEmailForm.ReloadDepartments();
+                        this.Close();
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("[NEW_EMAIL_FORM] " + ex.Message.ToString());
+                if (MessageBox.Show("Xảy ra lỗi khi tạo email", "Message", MessageBoxButtons.OK, MessageBoxIcon.Error) == DialogResult.OK)
+                {
+                    this.Close();
+                }
+            }
+        }
+
+        private void btnClose_Click(object sender, EventArgs e)
+        {
+            _deptRepository.Dispose(); 
+            this.Close();
+        }
+
+        private void NewEmailForm_FormClosed(object sender, FormClosedEventArgs e)
+        {
+            _deptRepository.Dispose();
+        }
+    }
+}
