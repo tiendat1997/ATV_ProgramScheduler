20c4e3811a55ae8a03e93e7240e1495f829d33bf
Tín Nguyễn Trung <trungtin719.tt@gmail.com>
2019-02-18 21:22:44 +0700

    a

--- ATV.ProgramDept.DesktopApp/Program.cs
+++ ATV.ProgramDept.DesktopApp/Program.cs
@@ -18,8 +18,8 @@ namespace ATV.ProgramDept.DesktopApp
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            
-            Application.Run(new LoginForm());            
+            Application.Run(new StaticProgramForm());
+            //Application.Run(new LoginForm());            
         }
     }
 }
