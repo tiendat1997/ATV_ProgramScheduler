2b9c849d407ad869018aaa0d5efb69dcea96a04b
Nguyen Thuy Ngoc <ngocnt1808@gmail.com>
2019-02-22 20:48:46 +0700

    fix estimate start time

--- ATV.ProgramDept.Service/Utilities/ScheduleUlities.cs
+++ ATV.ProgramDept.Service/Utilities/ScheduleUlities.cs
@@ -23,18 +23,14 @@ namespace ATV.ProgramDept.Service.Utilities
                 int minutes = 0;
                 foreach (ScheduleBase schedule in schedules)
                 {
-                    if (prev.Hours + hours > 23)
-                    {
-                        hours = hours - 24;
-                    }
                     schedule.StartTime = prev.Add(new TimeSpan(hours,minutes,(int)seconds));
-                    
+                    if (schedule.StartTime.Days > 0)
+                    {
+                        schedule.StartTime = schedule.StartTime.Subtract(new TimeSpan(1, 0, 0, 0));
+                    }
+
                     seconds = (int)((schedule.Duration - (int)schedule.Duration) * 100);
                     hours = (int)schedule.Duration / 60;
-                    if (hours > 23)
-                    {
-                        hours = hours - 24;
-                    }
                     minutes = (int)schedule.Duration % 60;
 
                     prev = schedule.StartTime;
