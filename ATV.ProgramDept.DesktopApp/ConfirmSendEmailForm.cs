18edc31378b0116947f3988b401b1ce2617cb724
trungtin719tt <33480170+trungtin719tt@users.noreply.github.com>
2019-04-21 21:31:08 +0700

    commit before merge with dat

--- ATV.ProgramDept.DesktopApp/ConfirmSendEmailForm.cs
+++ ATV.ProgramDept.DesktopApp/ConfirmSendEmailForm.cs
@@ -1,4 +1,5 @@
 ﻿using ATV.ProgramDept.Service.Utilities;
+using ATV.ProgramDept.Service.ViewModel;
 using System;
 using System.Collections.Generic;
 using System.ComponentModel;
@@ -14,12 +15,14 @@ namespace ATV.ProgramDept.DesktopApp
     public partial class ConfirmSendEmailForm : Form
     {
         SendEmailForm parent;
+        private List<ScheduleViewModel> _scheduleViewModels;
         public ConfirmSendEmailForm(SendEmailForm parent)
         {
 
             this.parent = parent;
             InitializeComponent();
             InitData();
+            _scheduleViewModels = scheduleViewModels;
         }
 
         void InitData()
@@ -35,6 +38,41 @@ namespace ATV.ProgramDept.DesktopApp
             var result = MailUtils.SendEmailAsync(txtEmail.Text, txtSubject.Text, rtbContent.Text);
             MessageBox.Show("Đã gửi email tới " + parent.Department.Name);
             this.Close();
+        }
+
+        private List<ScheduleViewModel> GetExportSchedule()
+        {
+            List<ScheduleViewModel> exportSchedule = new List<ScheduleViewModel>();
+            if (parent.DateListChecked[0])
+            {
+                exportSchedule.Add(_scheduleViewModels.Where(s => s.DayOfWeek == (int)DayOfWeek.Monday).FirstOrDefault());
+            }
+            if (parent.DateListChecked[1])
+            {
+                exportSchedule.Add(_scheduleViewModels.Where(s => s.DayOfWeek == (int)DayOfWeek.Tuesday).FirstOrDefault());
+            }
+            if (parent.DateListChecked[2])
+            {
+                exportSchedule.Add(_scheduleViewModels.Where(s => s.DayOfWeek == (int)DayOfWeek.Wednesday).FirstOrDefault());
+            }
+            if (parent.DateListChecked[3])
+            {
+                exportSchedule.Add(_scheduleViewModels.Where(s => s.DayOfWeek == (int)DayOfWeek.Thursday).FirstOrDefault());
+            }
+            if (parent.DateListChecked[4])
+            {
+                exportSchedule.Add(_scheduleViewModels.Where(s => s.DayOfWeek == (int)DayOfWeek.Friday).FirstOrDefault());
+            }
+            if (parent.DateListChecked[5])
+            {
+                exportSchedule.Add(_scheduleViewModels.Where(s => s.DayOfWeek == (int)DayOfWeek.Saturday).FirstOrDefault());
+            }
+            if (parent.DateListChecked[6])
+            {
+                exportSchedule.Add(_scheduleViewModels.Where(s => s.DayOfWeek == (int)DayOfWeek.Sunday).FirstOrDefault());
+            }
+
+            return exportSchedule;
         }
     }
 }
